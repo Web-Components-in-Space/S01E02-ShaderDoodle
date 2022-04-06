67eccc794c0c9edec35ff240d2635d3b78b81be4
Ben Farrell <ben@benfarrell.com>
2022-04-05 22:27:00 -0700

    final setup merged from step 7

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,12 +1,10 @@
 import litcss from 'rollup-plugin-lit-css';
 import commonjs from '@rollup/plugin-commonjs';
-import nodepolyfill from 'rollup-plugin-node-polyfills';
 import { prepend } from 'rollup-plugin-insert'
 import { fromRollup } from '@web/dev-server-rollup';
 
 const pluginLitCSS = fromRollup(litcss);
 const pluginCommonJS = fromRollup(commonjs);
-const pluginNodePolyfill = fromRollup(nodepolyfill);
 const pluginPrepend = fromRollup(prepend);
 
 export default {
@@ -14,13 +12,14 @@ export default {
         '**/*.css': 'js'
     },
     open: true,
-    nodeResolve: true,
+    nodeResolve: {
+        exportConditions: ['production']
+    },
     plugins: [
         pluginLitCSS(),
         pluginPrepend('var remaining, curPixel, n_bits; \n\n', { include: './node_modules/gifjs/src/LZWEncoder.js'}),
         pluginCommonJS( {
             include: ['./node_modules/gifjs/**/*.js']
         }),
-        pluginNodePolyfill(),
     ]
 };
